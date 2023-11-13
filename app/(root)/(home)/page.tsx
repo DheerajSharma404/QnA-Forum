90aa924624c895b8fcf721d85723cb9f485bcf55
Dheeraj Sharma <at60sharwan@gmail.com>
2023-11-13 19:13:25 +0530

    Basic Folder structure

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=' relative min-h-screen dotted-background  '>
+      <h1>hello ji</h1>
+    </main>
+  );
+}
