91425ed1aceebc0144f04e792bd5305e9e9c9902
Dheeraj Sharma <at60sharwan@gmail.com>
2023-11-24 22:58:10 +0530

    new webhook secret

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/webhook",
-    "questions/:id",
-    "/topics",
-    "/topics/:id",
-    "/profile/:id",
-    "/community",
-  ],
+  publicRoutes: ["/", "/api/webhook"],
 });
 
 export const config = {
