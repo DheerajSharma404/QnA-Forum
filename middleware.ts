9a2a0f395ecec093cbdd9bec181ffc5c214fa227
Dheeraj Sharma <at60sharwan@gmail.com>
2023-11-25 15:26:40 +0530

    debug: debuggin

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,13 @@
-import { authMiddleware as clerkAuthMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
 
-export const authMiddleware = (options: any) => {
-  const middleware = clerkAuthMiddleware(options);
-
-  return async (req: any, res: any, next: any) => {
-    console.log("Before authMiddleware");
-    try {
-      await middleware(req, res);
-      console.log("After authMiddleware");
-      next();
-    } catch (error) {
-      console.error("Error in authMiddleware", error);
-      res.status(500).send("Error in authMiddleware");
-    }
-  };
-};
-
-export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook(.*)"],
-  ignoredRoutes: ["/api/webhook(.*)"],
+export const middleware = authMiddleware({
+  publicRoutes: ["/", "/api/webhook"],
+  ignoredRoutes: ["/api/webhook"],
 });
 
+console.log("middleware", middleware);
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+console.log("config", config);
